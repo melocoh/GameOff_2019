5bf20a611d8f52c66098693331888418517f61f4
Melody Oh <melody.oh1412@gmail.com>
2019-12-01 14:13:41 -0800

    Got the menu ui, quit function, instrctions, and portals in.

---
+++ TetrisQuest/Assets/PortalTransport.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PortalTransport : MonoBehaviour
+{
+    
+    public string sceneName;
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        SceneManager.LoadScene(sceneName, LoadSceneMode.Additive);
+    }
+}
